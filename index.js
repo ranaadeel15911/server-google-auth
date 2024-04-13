edf82145bec4f9aa77260ab51b1ecd5d670142e0
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-04-13 19:41:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -83,6 +83,5 @@ app.post("/google-auth", async (req, res) => {
 app.get('/get',(req,res)=>{
 res.cookie("name","adeel").json({message:"All is Ok"})
 })
-    app.listen(PORT, ('/',(req,res) =>
-      return res.json({message:"Everything is Good"})
+    app.listen(PORT, () =>
         console.log(`Server running on PORT ${PORT}`));
